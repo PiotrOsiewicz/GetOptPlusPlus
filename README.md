59efd2bcb51f197ee5d19a5ce279b265aa4c01c6
Piotr Osiewicz <peterosiewicz@gmail.com>
2017-07-18 20:05:35 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+## GetOpt++
+### C++ Library for command-line argument parsing
+#### Current version: 0.1.0
+
+GetOpt++ is a small library for command-line argument parsing. It is
+lightweight and performance-focused.
+
+### Usage
+
+Include getopt++.h in your source file and construct the GOppParser with argc
+as first parameter and argv as second parameter.
+
+### FAQ
+Yet to be added.
---
+++ src/getopt++.h
@@ -0,0 +1,91 @@
+/* GetOpt++ - a lightweight parser for command-line arguments.
+ * Author: Piotr Osiewicz
+ * License: MIT
+ * Usage: Include this file in your project and initialize GOppParser
+ */
+
+#include <vector>
+#include <string>
+#include <memory>
+#include <stdexcept>
+
+class Argument{
+	public:
+		const std::string& GetName(void) const;
+		const int& GetIndex(void) const;
+		Argument(int index,std::string n_Name): Index(index), Name(n_Name) {};
+	private:
+		int Index; 
+		std::string Name;
+	
+};
+
+class Command: public Argument{
+	public:
+		const std::string& GetName(void) const;
+		const int& GetIndex(void) const;
+		virtual void Execute(void) = 0;
+	private:
+		int Index;
+		std::string Name;
+		std::vector<Argument*> Arguments;
+};
+
+class GOppParser{
+	public:
+		GOppParser(int argc, char** argv);
+		const int& GetArgCnt(void) const;
+		const std::vector<Argument*>& GetArgumentList(void) const;
+		const std::vector<Command*>& GetCommandList(void) const; 
+		const std::string& GetProgramName(void) const;
+	private:
+		void Parse(std::vector<Argument> Argv);
+		std::vector<Command*> CommandList;
+		std::vector<Argument*> ArgumentList;
+		int ArgCnt;
+		std::string ProgramName;
+};
+
+const std::string& Argument::GetName(void) const
+{
+	return Name;
+}
+
+const int& Argument::GetIndex(void) const
+{
+	return Index;
+}
+
+const int& GOppParser::GetArgCnt(void) const
+{
+	return ArgCnt;
+}
+
+const std::vector<Argument*>& GOppParser::GetArgumentList(void) const
+{
+	return ArgumentList;
+}
+const std::vector<Command*>& GOppParser::GetCommandList(void) const
+{
+	return CommandList;
+}
+
+const std::string& GOppParser::GetProgramName(void) const
+{
+	return ProgramName;
+}
+
+GOppParser::GOppParser(int argc, char **argv): ArgCnt(argc)
+{
+	if(argv == nullptr && argc < 1) {
+		throw std::invalid_argument("Argv");
+	} 
+	ProgramName = std::string(argv[0]);
+	std::vector<Argument*> ArgList(ArgCnt);
+	for(int i = 1;i<argc;i++){
+		std::string temp(argv[i]);
+		Argument *A = new Argument(i,temp);
+		ArgList.push_back(A);
+	}
+	ArgumentList = ArgList;
+}
