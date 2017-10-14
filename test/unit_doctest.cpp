df10c040cfd563203576e4fbbe023d46029f985c
Michele Adduci <adduci.michele@gmail.com>
2017-10-14 16:51:41 +0200

    Added example with doctest

--- test/unit_doctest.cpp
+++ test/unit_doctest.cpp
@@ -1,1 +1,10 @@
-#include "../src/getopt++.h"
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+#include "doctest.h"
+#include "getopt++.h"
+
+TEST_CASE("main")
+{
+  std::vector<char*> argv{"test","test2"};
+  GOpp::Parser CommandLine(1,argv.data());
+}
